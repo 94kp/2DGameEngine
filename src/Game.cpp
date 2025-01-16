01a6cf3cc7886047a431a46ece09d5b970783058
94kp <karan94@patil.com>
2025-01-16 20:44:57 +0530

    First commit

---
+++ src/Game.cpp
@@ -0,0 +1,103 @@
+#include "Game.h"
+#include <iostream>
+#include <SDL2/SDL.h>
+
+Game::Game()
+{
+  isRunning = false;
+  std::cout << "Constructor called" << std::endl; 
+}
+
+Game::~Game()
+{
+  std::cout << "Destructor called" << std::endl;
+}
+
+void Game::Initialise()
+{
+  // initialise window here
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+  {
+    std::cerr << "SDL initialisation failed" << std::endl;
+    return;
+  }
+  
+  window = SDL_CreateWindow(
+      "myEngine", 
+      SDL_WINDOWPOS_CENTERED, 
+      SDL_WINDOWPOS_CENTERED, 
+      800,
+      600,
+      SDL_WINDOW_BORDERLESS
+      ); // raw pointer to a struct
+      
+  if (!window)
+  {
+    std::cerr << "Error creating SDL Window" << std::endl;
+    return;
+  }
+
+  renderer = SDL_CreateRenderer(
+      window,
+      -1,
+      0);
+  
+  if (!renderer)
+  {
+    std::cerr << "Error creating SDL renderer" << std::endl;
+    return;
+  }
+
+  isRunning = true;
+}
+
+void Game::Run()
+{
+  while (isRunning)
+  {
+    ProcessInput();
+    Update();
+    Render();
+  }
+}
+
+void Game::ProcessInput()
+{
+  SDL_Event sdlEvent;
+  while(SDL_PollEvent(&sdlEvent))
+  {
+    switch(sdlEvent.type)
+    {
+      case SDL_QUIT:
+        isRunning = false;
+        break;
+      case SDL_KEYDOWN:
+        if (sdlEvent.key.keysym.sym == SDLK_ESCAPE)
+        {
+          isRunning = false;
+        }
+        break;
+    }
+  };
+
+
+
+}
+
+void Game::Update()
+{
+
+}
+
+void Game::Render()
+{
+
+}
+
+void Game::Destroy()
+{
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyedWindow(window);
+  SDL_Quit();
+}
+
