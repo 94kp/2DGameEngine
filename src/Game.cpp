dd5aa91cbe99b72692ef0426b391bb7f76d67183
94kp <karan94@patil.com>
2025-01-22 23:15:37 +0530

    deltaTime implemented

--- src/Game.cpp
+++ src/Game.cpp
@@ -100,21 +100,23 @@ glm::vec2 playerVelocity;
 void Game::Setup()
 {
     playerPosition = glm::vec2(10.0, 20.0);
-    playerVelocity = glm::vec2(1.0, 0.0);
+    playerVelocity = glm::vec2(100.0, 0.0);
 }
 
 void Game::Update()
 {
     //while (!SDL_TICKS_PASSED(SDL_GetTicks(), millisecsPreviousFrame + MILLISECS_PER_FRAME));
+    // Commenting the next 3 lines (till SDL_Delay) will run the game with an uncapped framerate
     int timeToWait = MILLISECS_PER_FRAME - (SDL_GetTicks() - millisecsPreviousFrame);
-    
     if (timeToWait > 0 && timeToWait <= MILLISECS_PER_FRAME)
         SDL_Delay(timeToWait);
 
+    double deltaTime = (SDL_GetTicks() - millisecsPreviousFrame) / 1000.0;
+
     millisecsPreviousFrame = SDL_GetTicks();
 
-    playerPosition.x += playerVelocity.x;
-    playerPosition.y += playerVelocity.y;
+    playerPosition.x += playerVelocity.x * deltaTime;
+    playerPosition.y += playerVelocity.y * deltaTime;;
 }
 
 void Game::Render()
